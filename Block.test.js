45d067f9f5730fc9e61923c295598114dd2a4667
Jonathan Jacka <jonathanjacka@gmail.com>
2022-11-26 21:43:06 -0600

    Adds hash property to Block

--- Block.test.js
+++ Block.test.js
@@ -1,6 +1,8 @@
 const Block = require('./Block');
 const { GENESIS_DATA } = require('./config');
 
+const cryptoHash = require('./crypto-hash');
+
 describe('Block', () => {
   const timestamp = 'a date';
   const lastHash = 'Some last hash';
@@ -30,6 +32,34 @@ describe('Block', () => {
 
     it('returns the correct genesis data', () => {
       expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
+  });
+
+  describe('mineBlock()', () => {
+    const lastBlock = Block.genesis();
+    const data = 'mined data';
+    const minedBlock = Block.mineBlock({ lastBlock, data });
+
+    it('returns a Block instance', () => {
+      expect(minedBlock instanceof Block).toBe(true);
+    });
+
+    it('sets the `lastHash` to be the hash of the lastBlock', () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('sets the data correctly', () => {
+      expect(minedBlock.data).toEqual(data);
+    });
+
+    it('sets a timestamp', () => {
+      expect(minedBlock.timestamp).not.toEqual(undefined);
+    });
+
+    it('creates a SHA-256 `hash` based on proper inputs', () => {
+      expect(minedBlock.hash).toEqual(
+        cryptoHash(minedBlock.timestamp, lastBlock.hash, data)
+      );
     });
   });
 });
