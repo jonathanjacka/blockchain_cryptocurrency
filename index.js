f19439de050c3ec358023ac57c8a1bffc369f4b1
Jonathan Jacka <jonathanjacka@gmail.com>
2022-12-14 16:52:52 -0600

    Adds post request to add block to blockchain

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const dotenv = require('dotenv').config();
 const app = express();
 const blockchain = new Blockchain();
 
+app.use(express.json());
+
+//Routes
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+  const { data } = req.body;
+  blockchain.addBlock({ data });
+  res.redirect('/api/blocks');
 });
 
 const PORT = process.env.PORT || 5000;
