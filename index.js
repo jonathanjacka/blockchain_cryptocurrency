a1c765d4f0e01f7f10b07625b8f4d43446ef5868
Jonathan Jacka <jonathanjacka@gmail.com>
2023-01-04 17:02:10 -0600

    Adds peer start and broadcast chain on /api/mine

--- index.js
+++ index.js
@@ -21,10 +21,19 @@ app.get('/api/blocks', (req, res) => {
 app.post('/api/mine', (req, res) => {
   const { data } = req.body;
   blockchain.addBlock({ data });
+  pubsub.broadcastChain();
   res.redirect('/api/blocks');
 });
 
-const PORT = process.env.PORT || 5000;
+const DEFAULT_PORT = process.env.PORT || 5000;
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = Number(DEFAULT_PORT) + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}, hello there...`);
 });
